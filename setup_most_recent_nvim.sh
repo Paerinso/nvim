2f1415fa4aa5e55d3460affdc511130aefe1fc63
Patrik <57088681+Paerinso@users.noreply.github.com>
2022-11-15 17:09:13 +0100

    Update setup_most_recent_nvim.sh

--- setup_most_recent_nvim.sh
+++ setup_most_recent_nvim.sh
@@ -19,14 +19,14 @@ else
     sudo tar xzvf /bin/nvim/nvim-linux64.tar.gz
     sudo rm -f nvim-linux64.tar.gz
 fi
-echo "do you want to download the github nvim config?"
-read downloadconfig
+#echo "do you want to download the github nvim config?"
+#read downloadconfig
 
-if [ $downloadconfig == "y" ] || [ $downloadconfig == "yes" ]
-then
-    echo "Downloading your config now."
-else 
-    echo "Okay install is complete without config."
-fi
-sleep 5
-/bin/nvim/nvim-linux64/bin/nvim
+#if [ $downloadconfig == "y" ] || [ $downloadconfig == "yes" ]
+#then
+#    echo "Downloading your config now."
+#else 
+#    echo "Okay install is complete without config."
+#fi
+#sleep 5
+#/bin/nvim/nvim-linux64/bin/nvim
